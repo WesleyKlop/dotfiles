925910b57072b140a7da5fb5d9d643868111ee31
Wesley Klop <wesley19097@gmail.com>
2021-01-27 17:25:23 +0100

    Add step for downloading extra packages during install

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+echo "Installing required packages"
+
+git clone https://github.com/lukechilds/zsh-nvm "$ZSH_CUSTOM/plugins/zsh-nvm"
+
 echo "Installing dotfiles using stow."
 
 stow -t "$HOME" git gpg ssh tmux vim zsh
