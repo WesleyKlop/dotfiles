06c04a0f13921c094ac149723ef941ada4639d39
Wesley Klop <wesley19097@gmail.com>
2021-07-20 10:09:09 +0200

    Update dot_zprofile.tmpl Update private_dot_npmrc.tmpl Add run_once_before_install-1password.sh.tmpl Remove run_once_install-1password.sh Rename run_once_install-antigen.sh to run_once_before_install-antigen.sh

--- run_once_install-1password.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -Lo /tmp/1password.zip  "https://cache.agilebits.com/dist/1P/op/pkg/v1.10.3/op_linux_amd64_v1.10.3.zip"
-unzip -o /tmp/1password.zip op -d ~/.local/bin
-rm /tmp/1password.zip
