73b58c778ab4fc9531bc140b02164578429e9223
Wesley Klop <wesley19097@gmail.com>
2021-07-27 09:10:24 +0200

    Improve scripts

--- run_once_before_install-antigen.sh
+++ run_once_before_install-antigen.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 mkdir -p "$HOME/.local/antigen"
-curl -o "$HOME/.local/antigen/antigen.zsh" -L https://git.io/antigen 
+curl -o "$HOME/.local/antigen/antigen.zsh" -L https://git.io/antigen
