292c2b95f95a24c0a4bea97608fd94692f10c1c0
Wesley Klop <wesley19097@gmail.com>
2021-08-08 21:45:19 +0200

    Don't assume GNUPGHOME is set

--- run_once_after_migrate-gnupg-folder.sh
+++ run_once_after_migrate-gnupg-folder.sh
@@ -2,7 +2,7 @@
 
 if [ -d "$HOME/.gnupg" ]; then
     killall gpg-agent
-    mv "$HOME/.gnupg" "$GNUPGHOME"
+    mv "$HOME/.gnupg" "${GNUPGHOME-$HOME/.local/gnupg}"
 else
     echo "GnuPG folder already migrated or not found."
 fi
