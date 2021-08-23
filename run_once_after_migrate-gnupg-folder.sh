140005e5e2e88601a5c87392f5f335730054a5b6
Wesley Klop <wesley19097@gmail.com>
2021-08-23 10:16:10 +0200

    Improve scripts

--- run_once_after_migrate-gnupg-folder.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d "$HOME/.gnupg" ]; then
-    killall gpg-agent
-    mv "$HOME/.gnupg" "${GNUPGHOME-$HOME/.local/gnupg}"
-else
-    echo "GnuPG folder already migrated or not found."
-fi
