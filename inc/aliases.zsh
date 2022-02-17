212b6d639880581780284942352be636404edcdb
Wesley Klop <wesley@q42.nl>
2022-02-17 13:20:11 +0100

    Split aliases/opts from zshrc

---
+++ inc/aliases.zsh
@@ -0,0 +1,12 @@
+alias dirs="dirs -v"
+alias artisan="php artisan"
+
+alias top="htop"
+alias diff="diff --color=auto"
+alias ip="ip -color=auto"
+
+alias ls="exa" 
+alias la="exa -lah"
+alias ll="exa -l"
+
+alias grep="rg"
