74465569b6ca3ce6a52f85f1776f088e292e2379
Wesley Klop <wesley19097@gmail.com>
2022-05-23 09:10:39 +0200

    Improve aliases

--- inc/aliases.zsh
+++ inc/aliases.zsh
@@ -2,11 +2,17 @@ alias dirs="dirs -v"
 alias artisan="php artisan"
 
 alias top="htop"
-alias diff="diff --color=auto"
 alias ip="ip -color=auto"
 
 alias ls="exa" 
 alias la="exa -lah"
-alias ll="exa -l"
+alias ll="exa -lh"
+alias lag="exa -lah --grid"
+alias llg="exa -lh --grid"
 
 alias grep="rg"
+
+alias cat="bat"
+
+alias nano="vim"
+alias emacs="vim"
