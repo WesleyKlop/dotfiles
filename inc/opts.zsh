7ec34797ad52b1f6f3bc0a5618517014f7a863d5
Wesley Klop <wesley@q42.nl>
2022-08-31 10:30:43 +0200

    Set better history options

--- inc/opts.zsh
+++ inc/opts.zsh
@@ -7,4 +7,7 @@ setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt share_history          # share command history data
+setopt inc_append_history     # Directly add to history instead of on shell exit
+setopt hist_find_no_dups      # Do not find duplicates
+
 
